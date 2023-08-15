a4d95991da58125d108a4e3435f2d453c87cf88f
Avijit karmaker <ajkacca@gmail.com>
2023-08-15 15:09:00 +0300

    update components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import ButtonContainer from "./components/ButtonContainer";
+import InfoContainer from "./components/InfoContainer";
 function App() {
-
-
   return (
     <>
+      <ButtonContainer />
+      <InfoContainer />
     </>
   );
 }
