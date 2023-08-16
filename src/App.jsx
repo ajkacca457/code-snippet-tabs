4ca1328ef5cff95d7356b4ce973613b5f69c25e6
Avijit karmaker <ajkacca@gmail.com>
2023-08-16 09:23:48 +0300

    update component button

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { data as jobData } from './data';
 function App() {
 
   return (
-    <div className='max-w-[1200px] bg-slate-300 p-8 mx-auto grid grid-cols-8 gap-x-[10px]'>
+    <div className='max-w-[1200px] bg-slate-300 p-8 mx-auto grid grid-cols-8 gap-x-[20px]'>
       <h1 className='col-span-full min-h-[50px] text-center'>Tabs Content</h1>
       <ButtonContainer {...jobData} />
       <InfoContainer {...jobData} />
--- src/components/ButtonContainer.jsx
+++ src/components/ButtonContainer.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 
-const ButtonContainer = () => {
+const ButtonContainer = (props) => {
+    const companyNames = Object.values(props).map(company => company.name);
     return (
-        <div className='col-span-2'>ButtonContainer</div>
+        <div className='col-span-2'>
+            {companyNames.map((company, index) => {
+                return (
+                    <button className='block bg-orange-300 w-full rounded h-fit my-2 py-2 px-4' key={index}>{company}</button>
+                );
+            })}
+        </div>
     );
 };
 
